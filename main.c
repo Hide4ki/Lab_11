7d1c342cb53f973c501f0f498d3fb204171a7bb0
Hide4ki <32337381+Hide4ki@users.noreply.github.com>
2018-09-23 22:01:18 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+#define LEN 1000
+
+int main(void)
+{
+	HANDLE hStdout;
+	FILE *fin;
+	char line[LEN];
+	char word[LEN];
+	char *l;
+	char *w;
+	int f;
+	char *pt;
+	WORD Color;
+	hStdout = GetStdHandle (STD_OUTPUT_HANDLE);
+	Color = BACKGROUND_INTENSITY | BACKGROUND_GREEN | FOREGROUND_INTENSITY | FOREGROUND_RED;
+	if(!(fin = fopen("input.txt","r")))
+		return 1;
+	while(fgets(line,sizeof(line),fin))
+	{
+		l = line;
+		w = word;
+		*w = '\0';
+		f = 0;
+		while(*l)
+		{
+			if(*l=='\"')
+			{
+				if(f)
+				{
+					*w++ = *l++;
+					*w = '\0';
+					SetConsoleTextAttribute (hStdout, Color );	
+					printf("%s",word);				
+					f = 0;
+					w = word;
+					*w = '\0';
+				}
+				else
+				{
+					*w = '\0';
+					SetConsoleTextAttribute (hStdout, 7 );
+					printf("%s",word);
+					f = 1;
+					w = word;
+					*w = '\0';
+				}
+			}
+			*w++ = *l++;
+		}
+		if(w)
+		{
+			*w = '\0';
+			SetConsoleTextAttribute (hStdout, 7 );
+			printf("%s",word);
+			f = 1;
+			w = word;
+			*w = '\0';
+		}
+	}
+	SetConsoleTextAttribute (hStdout, 7 );
+	getchar();
+	return 0;
+}
